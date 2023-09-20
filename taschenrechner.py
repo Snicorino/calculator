765a0cd02327e185740d7182a47e2c982a7ac870
Snicorino <92532099+Snicorino@users.noreply.github.com>
2023-09-20 21:18:03 +0200

    init

---
+++ taschenrechner.py
@@ -0,0 +1,12 @@
+a=int(input("a: "))
+b=int(input("b: "))
+c = input("Operator: (+, -, *, /): ")
+if (c=="+"):
+    d = a+b
+if (c=="-"):
+    d = a-b
+if (c=="*"):
+    d = a*b
+if (c=="/"):
+    d = a/b
+print (d)
